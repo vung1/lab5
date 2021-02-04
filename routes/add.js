b862713f83e675369f88a4333be67362e174e13a
Victor Ung <vung@ucsd.edu>
2021-02-04 00:40:45 -0800

    finished lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,11 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function (request, response) {
+    // Your code goes here
+    const name = request.query.name;
+    const description = request.query.description;
+    const imageURL = "http://lorempixel.com/400/400/people";
+    const newFriend = { name, description, imageURL };
+    data.friends.push(newFriend);
+    response.render("index", data);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // Get all of our friend data
-var data = require('../data.json');
+var data = require("../data.json");
 
-exports.view = function(request, response){
-	console.log(data);
-	response.render('index');
+exports.view = function (request, response) {
+    console.log(data);
+    response.render("index", data);
 };
